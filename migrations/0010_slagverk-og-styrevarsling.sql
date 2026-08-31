f21a5a456d08e761f163a5bc4cd0492a6960cced
saynain <sindre.ryland@gmail.com>
2026-08-31 14:01:32 +0200

    Slå sammen migrasjonene fra slagverk og styrevarsling til 0010

---
+++ migrations/0010_slagverk-og-styrevarsling.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `notification_preferences` ADD `board_tasks` text DEFAULT 'all' NOT NULL;--> statement-breakpoint
+ALTER TABLE `project_works` ADD `percussion_setup` text;--> statement-breakpoint
+ALTER TABLE `projects` ADD `percussion_notes` text;
